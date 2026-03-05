bae31e8cda09b02fedbd50307ef43b3397b66014
robertoalvarezm <roberto.carlos.avarez@gmail.com>
2026-03-05 13:35:57 -0600

    First

--- mi_app/app.R
+++ mi_app/app.R
@@ -13,6 +13,8 @@ ui <- fluidPage(
       plotOutput("grafico")
     )
   )
+  
+  
 )
 
 server <- function(input, output) {
@@ -24,4 +26,30 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
+options(download.file.method = "curl")
 shinylive::export("mi_app", "docs")
+
+
+
+git init
+git add .
+git commit -m "primera app shinylive"
+git remote add origin https://github.com/tu_usuario/mi-shiny-app.git
+git push -u origin main
+```
+
+### 5. Activa GitHub Pages
+
+
+#git init
+#git add .
+#git commit -m "primera app shinylive"
+#git remote add origin https://github.com/tu_usuario/mi-shiny-app.git
+#git push -u origin main
+
+#En tu repo de GitHub:
+# **Settings → Pages → Source → Deploy from branch → `main` → carpeta `/docs`**
+  
+  #En ~1 minuto tu app estará disponible en:
+  #```
+#https://tu_usuario.github.io/mi-shiny-app/
